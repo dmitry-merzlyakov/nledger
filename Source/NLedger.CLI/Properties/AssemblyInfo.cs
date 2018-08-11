4b9ba62ff661892920a08f98c22e41d9c4e0a3c2
dmitry-merzlyakov <dmitry.merzlyakov@gmail.com>
2018-08-12 01:15:43 +0300

    Publishing NLedger 0.7

--- Source/NLedger.CLI/Properties/AssemblyInfo.cs
+++ Source/NLedger.CLI/Properties/AssemblyInfo.cs
@@ -39,5 +39,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")][assembly: AssemblyVersion("0.7.*")]
-[assembly: AssemblyInformationalVersion("0.7 Public Beta (DEV Preview)")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyInformationalVersion("0.7 Public Beta")]
