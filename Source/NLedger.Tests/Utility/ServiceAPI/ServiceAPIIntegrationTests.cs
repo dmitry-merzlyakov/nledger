a76360d6f93046e50f55c5927187f95542c3913e
Dmitry Merzlyakov <dmitry_merzlyakov@hotmail.com>
2020-08-08 01:20:23 +0300

    Multitarget: adapting build scripts

--- Source/NLedger.Tests/Utility/ServiceAPI/ServiceAPIIntegrationTests.cs
+++ Source/NLedger.Tests/Utility/ServiceAPI/ServiceAPIIntegrationTests.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// **********************************************************************************
+// Copyright (c) 2015-2020, Dmitry Merzlyakov.  All rights reserved.
+// Licensed under the FreeBSD Public License. See LICENSE file included with the distribution for details and disclaimer.
+// 
+// This file is part of NLedger that is a .Net port of C++ Ledger tool (ledger-cli.org). Original code is licensed under:
+// Copyright (c) 2003-2020, John Wiegley.  All rights reserved.
+// See LICENSE.LEDGER file included with the distribution for details and disclaimer.
+// **********************************************************************************
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NLedger.Utility.ServiceAPI;
 using System;
 using System.Collections.Generic;
