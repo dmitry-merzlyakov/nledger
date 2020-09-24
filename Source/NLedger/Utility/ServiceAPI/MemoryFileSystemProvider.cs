32ed769035ff7bea94c4ce5c2ea65e5fcd155c87
Dmitry Merzlyakov <dmitry_merzlyakov@hotmail.com>
2020-09-24 08:24:01 -0500

    Fixing build issues

--- Source/NLedger/Utility/ServiceAPI/MemoryFileSystemProvider.cs
+++ Source/NLedger/Utility/ServiceAPI/MemoryFileSystemProvider.cs
@@ -1,4 +1,12 @@
-﻿using NLedger.Abstracts;
+﻿// **********************************************************************************
+// Copyright (c) 2015-2020, Dmitry Merzlyakov.  All rights reserved.
+// Licensed under the FreeBSD Public License. See LICENSE file included with the distribution for details and disclaimer.
+// 
+// This file is part of NLedger that is a .Net port of C++ Ledger tool (ledger-cli.org). Original code is licensed under:
+// Copyright (c) 2003-2020, John Wiegley.  All rights reserved.
+// See LICENSE.LEDGER file included with the distribution for details and disclaimer.
+// **********************************************************************************
+using NLedger.Abstracts;
 using System;
 using System.Collections.Generic;
 using System.IO;
