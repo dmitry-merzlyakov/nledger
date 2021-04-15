0d417e86563b780a358e2f6652019e96c850fc5f
Dmitry Merzlyakov <dmitry_merzlyakov@hotmail.com>
2021-04-15 13:01:06 -0500

    Added Python Integration project

---
+++ Source/NLedger.Extensibility.Python/PythonModule.cs
@@ -0,0 +1,93 @@
+﻿using NLedger.Expressions;
+using NLedger.Scopus;
+using NLedger.Utils;
+using NLedger.Values;
+using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLedger.Extensibility.Python
+{
+    /// <summary>
+    /// Ported from python_module_t
+    /// </summary>
+    public class PythonModule : Scope
+    {
+        public PythonModule(PythonSession pythonSession, string name)
+        {
+            PythonSession = pythonSession;
+            ModuleName = name;
+            ModuleGlobals = new PyDict();
+            ImportModule(name);
+        }
+
+        public PythonModule(PythonSession pythonSession, string name, PyModule obj)
+        {
+            PythonSession = pythonSession;
+            ModuleName = name;
+            ModuleObject = obj;
+            ModuleGlobals = (PyDict)obj.GetAttr("__dict__");
+        }
+
+        public PythonSession PythonSession { get; }
+        public string ModuleName { get; }
+        public PyModule ModuleObject { get; private set; }
+        public PyDict ModuleGlobals { get; private set; }
+
+        public override string Description => ModuleName;
+
+        public void DefineGlobal(string name, PyObject obj)
+        {
+            ModuleGlobals[name] = obj;
+        }
+
+        public void ImportModule(string name, bool importDirect = false)
+        {
+            var mod = Py.Import(name) ?? throw new RuntimeError($"Module import failed (couldn't find {name})");
+            var globals = mod.GetAttr("__dict__") ?? throw new RuntimeError($"Module import failed (couldn't find {name})");
+
+            if (!importDirect)
+            {
+                ModuleObject = mod;
+                ModuleGlobals = (PyDict)globals;
+            }
+            else
+            {
+                ModuleGlobals.Update(mod.GetAttr("__dict__"));
+            }
+        }
+
+        public override ExprOp Lookup(SymbolKindEnum kind, string name)
+        {
+            if (kind == SymbolKindEnum.FUNCTION)
+            {
+                Logger.Current.Debug("python.interp", () => $"Python lookup: {name}");
+                if (ModuleGlobals.HasKey(name))
+                {
+                    var obj = ModuleGlobals.GetItem(name);
+                    if (obj != null)
+                    {
+                        var objModule = obj as PyModule;
+                        if (objModule != null)
+                        {
+                            var pythonModule = PythonSession.GetOrCreateModule(objModule, name);
+                            return ExprOp.WrapValue(Value.ScopeValue(pythonModule));
+                        }
+                        else
+                        {
+                            return ExprOp.WrapFunctor(GetFunctor(obj, name));
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private ExprFunc GetFunctor(PyObject obj, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/NLedger.Extensibility.Python/PythonSession.cs
@@ -0,0 +1,92 @@
+﻿using NLedger.Expressions;
+using NLedger.Scopus;
+using NLedger.Utility;
+using NLedger.Utils;
+using NLedger.Values;
+using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLedger.Extensibility.Python
+{
+    public class PythonSession : ExtendedSession
+    {
+        public bool IsSessionInitialized { get; private set; }
+        public PythonModule MainModule { get; private set; }
+        public IDictionary<PyModule, PythonModule> ModulesMap { get; } = new Dictionary<PyModule, PythonModule>();
+
+        public PythonModule GetOrCreateModule(PyModule pyModule, string name)
+        {
+            PythonModule pythonModule;
+            if (!ModulesMap.TryGetValue(pyModule, out pythonModule))
+                ModulesMap.Add(pyModule, pythonModule = new PythonModule(this, name, pyModule));
+
+            return pythonModule;
+        }
+
+        public override void DefineGlobal(string name, object value)
+        {
+            MainModule.DefineGlobal(name, PyObject.FromManagedObject(value));
+        }
+
+        public override void Eval(string code, ExtensionEvalModeEnum mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ImportOption(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize()
+        {
+            if (IsInitialized())
+                return;
+
+            var trace = Logger.Current.TraceContext("python_init", 1)?.Message("Initialized Python").Start(); // TRACE_START
+
+            try
+            {
+                Logger.Current.Debug("python.interp", () => "Initializing Python");
+
+                if (!PythonEngine.IsInitialized)
+                    throw new InvalidOperationException("assert(Py_IsInitialized());");
+
+                MainModule = new PythonModule(this, "__main__", Py.Import("__main__"));
+                Py.Import("ledger");
+
+                IsSessionInitialized = true;
+            }
+            catch (Exception ex)
+            {
+                // TODO PyErr_Print();
+                ErrorContext.Current.AddErrorContext(ex.ToString());
+                throw new RuntimeError("Python failed to initialize");
+            }
+
+            trace?.Finish(); // TRACE_FINISH
+        }
+
+        public override bool IsInitialized()
+        {
+            return IsSessionInitialized;
+        }
+
+        public override Value PythonCommand(CallScope scope)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public override Value ServerCommand(CallScope args)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        protected override ExprOp LookupFunction(string name)
+        {
+            return MainModule.Lookup(Scopus.SymbolKindEnum.FUNCTION, name);
+        }
+    }
+}
